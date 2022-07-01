f99371769363650e76b8e9ee65b134b46c11688d
Arkadiusz Paszenda <aro2457@gmail.com>
2022-07-01 15:01:05 +0200

    values and variables assignments

---
+++ 01-Fundamentals-Part-1/practice/assignments.js
@@ -0,0 +1,7 @@
+let country = "Poland";
+let continent = "Europe";
+let population = 38;
+
+console.log("Country: ", country);
+console.log("Continent: ", continent);
+console.log("Population (in millions): ", population);
