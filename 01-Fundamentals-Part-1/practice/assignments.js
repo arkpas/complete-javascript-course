a1b52458a6cf5c9b0d63aac25fc93dc8dad9c12a
Arkadiusz Paszenda <aro2457@gmail.com>
2022-07-01 15:44:44 +0200

    data types + const, let and var + assignments

--- 01-Fundamentals-Part-1/practice/assignments.js
+++ 01-Fundamentals-Part-1/practice/assignments.js
@@ -1,7 +1,24 @@
-let country = "Poland";
-let continent = "Europe";
+console.log("\n\n***** ASSIGNMENTS *****\n\n")
+// LECTURE: Values and Variables
+
+const country = "Poland";
+const continent = "Europe";
 let population = 38;
 
 console.log("Country: ", country);
 console.log("Continent: ", continent);
 console.log("Population (in millions): ", population);
+
+
+// LECTURE: Data Types
+
+const isIsIsland = false;
+
+console.log("Is island: ", isIsIsland);
+
+
+// LECTURE: let, const and var
+
+const language = "Polski";
+console.log("Language: ", language);
+
--- 01-Fundamentals-Part-1/practice/script.js
+++ 01-Fundamentals-Part-1/practice/script.js
@@ -1,3 +1,4 @@
+/*
 let js = "amazing";
 
 console.log(40 + 8 + 23 - 10);
@@ -10,3 +11,48 @@ console.log(firstName);
 console.log(firstName);
 console.log(firstName);
 console.log(firstName);
+
+
+// ----- DATA TYPES -----
+let jsIsFun = true;
+console.log("jsIsFun:", jsIsFun);
+
+// console.log(typeof true);
+console.log("type:", typeof jsIsFun);
+// console.log(typeof 23);
+// console.log(typeof "Jonas");
+
+jsIsFun = "Sure";
+console.log("jsIsFun:", jsIsFun);
+console.log("type:", typeof jsIsFun);
+
+let year;
+console.log("year:", year);
+console.log("type:", typeof year);
+
+year = 1995;
+console.log("year:", year);
+console.log("type:", typeof year);
+
+let nullVar = null;
+console.log("nullVar:", nullVar);
+console.log("type:", typeof nullVar);
+
+*/
+
+// ----- let, const and var -----
+
+let age = 30;
+age = 31;
+
+const birthYear = 1995;
+// birthYear = 12;
+// const job;
+
+var job = "programmer";
+job = "jobless";
+console.log("Job: ", job);
+
+lastName = "Paszenda";
+console.log("Last name (global variable): ", lastName);
+
