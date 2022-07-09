71e325653c19f6259ccb041cae15a1289c80b977
Arkadiusz Paszenda <aro2457@gmail.com>
2022-07-09 11:16:34 +0200

    coding challenge 1

---
+++ 01-Fundamentals-Part-1/practice/challenges.js
@@ -0,0 +1,27 @@
+console.log("\n\n***** CHALLENGES *****\n\n")
+// Coding Challenge #1
+
+const markWeight1 = 78;
+const markHeight1 = 1.69;
+const johnWeight1 = 92;
+const johnHeight1 = 1.95;
+
+const markBMI1 = markWeight1 / markHeight1 ** 2;
+const johnBMI1 = johnWeight1 / johnHeight1 ** 2;
+const isMarkBMIHigher1 = markBMI1 > johnBMI1;
+console.log("Mark BMI 1:", markBMI1);
+console.log("John BMI 1:", johnBMI1);
+console.log("Is Mark's BMI higher than John:", isMarkBMIHigher1)
+
+
+const markWeight2 = 95;
+const markHeight2 = 1.88;
+const johnWeight2 = 85;
+const johnHeight2 = 1.76;
+
+const markBMI2 = markWeight2 / markHeight2 ** 2;
+const johnBMI2 = johnWeight2 / johnHeight2 ** 2;
+const isMarkBMIHigher2 = markBMI2 > johnBMI2;
+console.log("Mark BMI 2:", markBMI2);
+console.log("John BMI 2:", johnBMI2);
+console.log("Is Mark's BMI higher than John:", isMarkBMIHigher2)
